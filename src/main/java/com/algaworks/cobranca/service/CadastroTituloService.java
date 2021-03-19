3befbd8276d1b616388eecc7a18e74d4f44b28e6
bomcabelo <rodrigues.silva.anderson@gmail.com>
2021-03-19 08:59:04 -0300

    Recebendo o título com Ajax

--- src/main/java/com/algaworks/cobranca/service/CadastroTituloService.java
+++ src/main/java/com/algaworks/cobranca/service/CadastroTituloService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 
@@ -24,6 +25,14 @@ public class CadastroTituloService {
 	public void excluir(Long codigo) {
 		titulos.deleteById(codigo);
 		
+	}
+
+	public String receber(Long codigo) {
+		Titulo titulo = titulos.getOne(codigo);
+		titulo.setStatus(StatusTitulo.RECEBIDO);
+		titulos.save(titulo);
+		
+		return StatusTitulo.RECEBIDO.getDescricao();
 	}
 	
 }
